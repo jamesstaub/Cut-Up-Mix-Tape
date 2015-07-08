a9eab0ba69269ba8fd8379cd79c5d5d0a0104729
jamesstaub <james.staub@gmail.com>
2015-07-08 16:37:55 -0400

    implements search requesat at api/query

--- public/js/services/CutUpService.js
+++ public/js/services/CutUpService.js
@@ -1,15 +1,13 @@
 angular.module('CutUpService', []).factory('CutUp', ['$http', function($http) {
 
     factory = {};
-
-        // call to get all nerds
     factory.get = function() {
         return $http.get('/api').success(function(response){
           angular.copy(response, factory.cutup)
-        })
+        });
     };
 
-    factory.cutup = []
+    factory.cutup = [];
 
     return factory;
 
--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,61 @@ var path = require('path');
 
 /********** genius api *********/
 var token = process.env.GENIUS_ACCESS_TOKEN ||  env(path.join(__dirname, '../.env'));
-var searchQuery = 'i got something to say i killed your baby today';
+// var searchQuery = 'i got something to say i killed your baby today  ';1
 // Instantiate a Genius instance:
 var Genius = require("node-genius");
 var geniusClient = new Genius(process.env.GENIUS_ACCESS_TOKEN);
 
-// Call functions on that instance:
-var geniusResults = [];
-geniusClient.search(searchQuery, function (error, results) {
-  if (error)
-    console.error("Whops. Something went wrong:", error);
-  else
-    var jsonResults = JSON.parse(results);
-    jsonResults.response.hits.forEach(function(h){
-      var song = { title: h.result.title, artist: h.result.primary_artist.name }
-      geniusResults.push(song);
+
+
+
+var searchGeniusPromise = function(searchQuery, transformData) {
+  return new Promise(function(resolve, reject) {
+
+    geniusClient.search(searchQuery, function(error, results) {
+      if (error) {
+        reject(error);
+        // console.error("Whops. Something went wrong:", error);
+      } else {
+        var jsonResults = JSON.parse(results);
+
+        // if there's a function as a second argument, use that to transform the data, otherwise just return the data
+        var transformed = transformData instanceof Function ? transformData(jsonResults) : jsonResults;
+
+        resolve(transformed);
+      }
     });
+  })
+}
 
-});
+function getTitleArtist(results){
+  var geniusResults = [];
+  results.response.hits.forEach(function(h) {
+    var song = {
+      title: h.result.title,
+      artist: h.result.primary_artist.name
+    }
+    geniusResults.push(song);
+  });
+  return geniusResults
+}
 
+
+
+function searchGenius(searchQuery){
+  // invoking the above promise with a transformer function as the second argument.
+
+  return searchGeniusPromise(searchQuery, getTitleArtist);
+}
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/:query', function(req, res) {
   // res.render('index', { title: 'Express', user: req.user });
-  res.json(geniusResults);
+
+  searchGenius(req.params.query).then(function(songsArray){
+    res.json(songsArray);
+  })
+
 });
 
 
