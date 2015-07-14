e7ac51ad1ab2e5f94b713898ac6a5c78f18873fc
jamesstaub <james.staub@gmail.com>
2015-07-13 22:32:50 -0400

    cutups template getting data from db

--- public/js/controllers/CutupCtrl.js
+++ public/js/controllers/CutupCtrl.js
@@ -1,14 +1,12 @@
-// public/js/controllers/NerdCtrl.js
 angular.module('cutupApp').controller('CutUpController', function($scope, CutUp) {
 
   console.log("calls cutup ctrller");
 
   $scope.getCutups = function(){
-    Cutup.get().success(function(response){
+    CutUp.get().success(function(response){
       $scope.cutups = response;
     })
   }
-
 
 
 });
